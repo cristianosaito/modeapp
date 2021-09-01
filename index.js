40d7010d544f724961f36e1d04bc0c0424a87f00
cristianosaito <cristiano.saitodesouza@gmail.com>
2021-09-01 17:17:29 -0300

    msg pipe index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.get('/', function (req, res) {
-res.send('Hello F** World!');
+res.send('Hello F** World pipeline!');
 });
 app.listen(3000, function () {
  console.log('Servidor rodando na porta 3000!');
